013f855e5a55ea24a30e21b82de976ffbd43abdd
Wilbert Fundira <97061882+WillFundira@users.noreply.github.com>
2025-11-07 20:23:37 -0800

    Create Card.cpp

---
+++ Card.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <mutex>
+#include <iostream>
+#include <iostream>
+
+// This class simulates a regular card from a deck if playing cards,
+class Card {
+    public Card();
+
+};
